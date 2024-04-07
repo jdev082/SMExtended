beb1c930876416d6d0f45603116d65a08caf44ad
jdev082 <jdev082@jdev.eu.org>
2024-04-07 13:30:58 -0500

    fix: remove customization/options as its hindering development

--- extend.js
+++ extend.js
@@ -3,24 +3,7 @@ const bottom = document.getElementById("bottom")
 const canvas = document.getElementById("instrument-canvas")
 const tempoSlider = document.getElementsByName("tempo")
 
-const colorfix = "#topbar { filter: invert(100); }"
-style = document.createElement('style')
-style.innerHTML = colorfix
-document.head.appendChild(style)
-
-let color = browser.storage.sync.get("color");
-color.then(setBackground, () => {return})
-
-let tlimit = browser.storage.sync.get("tlimit");
-tlimit.then(setTLimit, (error) => {alert(error)});
-
-function setBackground(c) {
-    document.body.style.backgroundColor = c.color
-    bottom.style.backgroundColor = c.color
-}
-
-function setTLimit(t) {
-    tempoSlider[0].max = t.tlimit;
-}
-
 header.innerText = "Song Maker Extended"
+tempoSlider[0].max = t.tlimit;
+
+
