eb5fea94d3a69865678c0ccbe306032e752415d7
jdev082 <jdev082@jdev.eu.org>
2024-04-07 14:19:37 -0500

    feat: various features and improvements

--- extend.js
+++ extend.js
@@ -2,8 +2,30 @@ const header = document.getElementById("header-logo")
 const bottom = document.getElementById("bottom")
 const canvas = document.getElementById("instrument-canvas")
 const tempoSlider = document.getElementsByName("tempo")
+const rangeCover = document.getElementsByClassName('range-cover')[0]
+const settingsBtn = document.getElementById("settings-button")
 
 header.innerText = "Song Maker Extended"
-tempoSlider[0].max = t.tlimit;
+tempoSlider[0].max = "1000"
+rangeCover.childNodes[0].max = "350"
 
+function createBreak(parent = document, height = 1) {
+    var br = document.createElement("br")
+    br.style.lineHeight = height
+    parent.appendChild(br)
+}
 
+createBreak(rangeCover, 2.8)
+var tempoInput = document.createElement("input")
+tempoInput.placeholder = "Tempo (Precise)"
+tempoInput.addEventListener("change", () => {
+    tempoSlider[0].value = tempoInput.value
+})
+rangeCover.appendChild(tempoInput)
+
+//settingsBtn.addEventListener("click", () => {
+//    bars = document.getElementById("bars")
+//    bars.max = "9999"
+//})
+
+console.log(o)
